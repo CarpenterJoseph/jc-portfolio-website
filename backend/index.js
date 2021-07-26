dd2fc12a4f987670dbd5971761fbd617b06c7343
Joe <carpenter.joseph1998@gmail.com>
2021-07-26 16:47:40 +0200

    backend update

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 const Express = require('express')
 
+//database
 const DB = require('./DB')
 
 //config
 const app = Express()
 const port = 3000
 
+//middleware
 app.use(Express.json())
+
+//routes
+app.use("/api/projects", require("./routes/Projects"))
+
+
 
 const mongoURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/projects'
 DB.connect(mongoURL)
 
-require('./routes/Projects')(app)
+
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`)
--- backend/routes/Projects.js
+++ backend/routes/Projects.js
@@ -1,39 +1,38 @@
+const express = require('express')
+const router = express.Router()
 const Project = require('../models/Project')
 
-module.exports = function (app) {
-	//get all projects
-	app.get('/api/projects/', (req, res) => {
-		Project.find({}, (err, docs) => {
-			if (!err) {
-				res.send(docs)
-			} else {
-				res.send({error: err})
-			}
-		})
+//get all projects
+router.get('/', (req, res) => {
+	Project.find({}, (err, docs) => {
+		if (!err) {
+			res.send(docs)
+		} else {
+			res.send({error: err})
+		}
 	})
+})
 
-	app.get('/api/projects/:projectID', (req, res) => {
-		let projectID = req.params.projectID
-		Project.findById(projectID, (err, project) => {
-			if (!err) {
-				res.send(project)
-			} else {
-				res.send({error: err})
-			}
-		})
+router.get('/:projectID', (req, res) => {
+	let projectID = req.params.projectID
+	Project.findById(projectID, (err, project) => {
+		if (!err) {
+			res.send(project)
+		} else {
+			res.send({error: err})
+		}
 	})
+})
 
-	app.post('/api/projects/', (req, res) => {
-		let project = req.body
-
-		console.log(project)
-
-		Project.create(project, (err, project) => {
-			if (!err) {
-				res.send(project)
-			} else {
-				res.send({error: err})
-			}
-		})
+router.post('/', (req, res) => {
+	let project = req.body
+	Project.create(project, (err, project) => {
+		if (!err) {
+			res.send(project)
+		} else {
+			res.send({error: err})
+		}
 	})
-}
+})
+
+module.exports = router
