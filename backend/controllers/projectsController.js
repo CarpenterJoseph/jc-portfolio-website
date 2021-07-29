0c99c6cd08fc985792f2af9489651cf319599d8d
Joseph Carpenter <carpenter.joseph1998@gmail.com>
2021-07-29 18:15:06 +0200

    Update projectsController.js

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -39,43 +39,49 @@ router.post('/', (req, res) => {
 })
 
 //update user
-router.put('/:projectID', (req, res) => {
+router.put('/:projectID', async (req, res) => {
 	let projectID = req.params.projectID
 	let newProjectEdit = req.body
-	project.findById(projectID, (error, project) => {
-		console.log(error)
-		if (!error) {
-			project.name = newProjectEdit.name || project.name
-			project.description = newProjectEdit.description || project.description
-			project.picture_url = newProjectEdit.picture_url || project.picture_url
-			project.save((error, updatedProject) => {
-				if (!error) {
-					res.send(updatedProject)
-				} else {
-					res.send(error)
-				}
-			})
-		} else {
-			res.send({error: error})
-		}
-	})
+	try {
+		project.findById(projectID, (project) => {
+			
+			if (project) {
+				project.name = newProjectEdit.name || project.name
+				project.description = newProjectEdit.description || project.description
+				project.picture_url = newProjectEdit.picture_url || project.picture_url
+				project.save((error, updatedProject) => {
+					if (!error) {
+						res.send(updatedProject)
+					} else {
+						res.send(error)
+					}
+				})
+			}
+		})
+	} catch (error) {
+		res.send({error: error})
+	}
 })
 //delete user by id
 router.delete('/:projectID', (req, res) => {
 	let projectID = req.params.projectID
-	project.findById(projectID, (error, user) => {
-		if (!error) {
-			user.remove((error) => {
-				if (!error) {
-					res.send('PROJECT DELETED')
-				} else {
-					res.send({error: error})
-				}
-			})
-		} else {
-			res.send({error: error})
-		}
-	})
+	try {
+		project.findById(projectID, (error, project) => {
+			if (project) {
+				project.remove((error) => {
+					if (!error) {
+						res.send('PROJECT DELETED')
+					} else {
+						res.send({error: error})
+					}
+				})
+			} else {
+				res.send({error: error})
+			}
+		})
+	} catch (error) {
+		res.send({error: error})
+	}
 })
 
 module.exports = router
